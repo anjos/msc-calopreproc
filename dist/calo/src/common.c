7926bac46cfcf3174f92f5f8fb0eb768847db525
rabello <rabello@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2000-04-06 01:14:06 +0000

    I've corrected an error at the PhiWrap() function. It did not foresee the case where we do lie in the PhiWrap region but PhiMax is still greater than PhiMin. This may happen if L1 decides so.
    git-svn-id: file:///Users/andre/andre_msc/trunk/calo-preproc@29 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- dist/calo/src/common.c
+++ dist/calo/src/common.c
@@ -5,9 +5,19 @@
 
 const static double MaxPhiWindow = 2.0;
 
+/* This functions examins 2 cases. 1) The RoI falls into the phi wrap region,
+   but PhiMin is not greater than PhiMax. 2) The RoI falls into the phi wrap
+   region and PhiMin is greater than PhiMax. */
 Flag PhiWrap(double* PhiMax, double* PhiMin)
 {
-  if ( fabs(*PhiMax - *PhiMin) > MaxPhiWindow) {
+  if ( fabs(*PhiMax - *PhiMin) > MaxPhiWindow) { /* Yes is a PhiWrap case */
+    if(*PhiMax > *PhiMin) {
+      /* now we have to exchage phimax and phimin because the expected order is
+       inverted. If this is not correct, wrong results may arrive. */
+      double temp = *PhiMax;
+      *PhiMax = PhiMin;
+      *PhiMin = temp;
+    }
     *PhiMax = *PhiMax + 2 * PI;
     return(ON);
   }
