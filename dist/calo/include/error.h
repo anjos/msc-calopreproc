2af1cfeefcce5956cba75d12a8724392173fc3a0
rabello <rabello@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2000-05-26 17:19:17 +0000

    intermediate
    git-svn-id: file:///Users/andre/andre_msc/trunk/calo-preproc@53 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- dist/calo/include/error.h
+++ dist/calo/include/error.h
@@ -1,17 +1,11 @@
 /* Error logging
  *
- * $Log: error.h,v $
- * Revision 1.1.1.1  2000/03/13 21:03:45  rabello
- * Initial Release
- *
- * Revision 1.1.1.1  1998/07/09 16:09:08  rabello
- * Calorimeter C Library
- *
+ * $Id: error.h,v 1.2 2000/05/26 17:19:17 rabello Exp $
  */
 
-#ifndef ERROR_H
+#ifndef __ERROR_H
 
-#define ERROR_H
+#define __ERROR_H
 
 typedef enum ErrorCode {SUCCESS = 0, ERROR = 1} ErrorCode;
 
