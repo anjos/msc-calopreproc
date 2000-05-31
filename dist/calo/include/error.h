46edcd704651c9e42b465e8fb501a7f3db95c4e4
rabello <rabello@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2000-05-31 12:10:12 +0000

    Changed the ErrorCode literals to CALO_*
    git-svn-id: file:///Users/andre/andre_msc/trunk/calo-preproc@72 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- dist/calo/include/error.h
+++ dist/calo/include/error.h
@@ -1,13 +1,13 @@
 /* Error logging
  *
- * $Id: error.h,v 1.2 2000/05/26 17:19:17 rabello Exp $
+ * $Id: error.h,v 1.3 2000/05/31 12:10:12 rabello Exp $
  */
 
 #ifndef __ERROR_H
 
 #define __ERROR_H
 
-typedef enum ErrorCode {SUCCESS = 0, ERROR = 1} ErrorCode;
+typedef enum ErrorCode {CALO_SUCCESS = 0, CALO_ERROR = 1} ErrorCode;
 
 #endif
 
