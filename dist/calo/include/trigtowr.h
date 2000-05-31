d3cf3419d63a523942ba35d74878947c348d9092
rabello <rabello@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2000-05-31 13:57:12 +0000

    - Moved the includes from the header to the implementation file trigtowr.c. This was done to preserve integrity and encapsulation and also to improve readability - Changed name of door keepers - Added a $Id$ CVS flag
    git-svn-id: file:///Users/andre/andre_msc/trunk/calo-preproc@79 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- dist/calo/include/trigtowr.h
+++ dist/calo/include/trigtowr.h
@@ -1,27 +1,22 @@
-#ifndef TRIGTOWR_H
-#define TRIGTOWR_H
+/* Hello emacs, this is -*- c -*- */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+/* $Id: trigtowr.h,v 1.2 2000/05/31 13:57:12 rabello Exp $ */
+
+#ifndef _TRIGTOWR_H
+#define _TRIGTOWR_H
+
 #include "data.h"
 #include "error.h"
 #include "common.h"
-#include "portable.h"
 #include "ttdef.h"
-#include "zstt.h"
 
-/* static constants */
 const static double EMRoIGran = EMROIGRAN;
 const static int MaxNumberOfLayers = 8;
 const static double EtaTTSize = 0.1;
 const static double PhiTTSize = 0.1;
 
-/* const static double PhiTTSize = 2 * PI / 64; */
 
-ErrorCode BuildCaloTTS(const ROI*, const Flag, CaloTTEMRoI*);
+ErrorCode BuildCaloTTS(const ROI*, const bool_t, CaloTTEMRoI*);
 void FreeCaloEMRoI(CaloTTEMRoI*);
 
-extern double rint(double x);
-
-#endif
+#endif /* _TRIGTOWR_H */
