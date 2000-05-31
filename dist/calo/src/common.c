f4b3ac695d25e5ba46b5ec62100111c5f7168af4
rabello <rabello@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2000-05-31 12:09:36 +0000

    - Moved the comments about functions to the header file. - Eliminated the Flag type and converted all occurences into bool_t literals. - Changed the approach to PhiWrap(), simplifying it. (new drawing on doc/fig/phiwrap.fig explains it).
    git-svn-id: file:///Users/andre/andre_msc/trunk/calo-preproc@71 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- dist/calo/src/common.c
+++ dist/calo/src/common.c
@@ -1,4 +1,6 @@
-/* $Id: common.c,v 1.5 2000/04/07 18:58:43 rabello Exp $ */
+/* Hello emacs, this is -*- c -*- */
+
+/* $Id: common.c,v 1.6 2000/05/31 12:09:36 rabello Exp $ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,33 +9,24 @@
 
 double fabs(double x);
 
-const static double MaxPhiWindow = 2.0;
-
-/* The L1 RoI selector, may mark the value of PhiMax as being the max value of
-   phi among the corners of the RoI or the one in the end following the
-   counter-clockwise direction of the RoI along phi. This functions examins the
-   2 cases and correct the RoI phi values: 1st. case) If the RoI falls into the
-   phi wrap region, but PhiMin is not greater than PhiMax. 2nd. case) If the
-   RoI falls into the phi wrap region and PhiMin is greater than PhiMax. In
-   both cases, the function adjusts PhiMin to be the greatest of the two values
-   and add 2*PI to PhiMax. This way, PhiMax goes to a value, greater than
-   PhiMin, which would not happen if this verification was not done here. */
-Flag PhiWrap(double* PhiMax, double* PhiMin)
+bool_t PhiWrap(double* max, double* min)  
 {
-  if ( fabs(*PhiMax - *PhiMin) > MaxPhiWindow) { /* Yes is a PhiWrap case */
-    if(*PhiMax > *PhiMin) {
-      /* now we have to exchage phimax and phimin because the expected order is
-       inverted. If this is not correct, wrong results may arrive. */
-      double temp = *PhiMax;
-      *PhiMax = *PhiMin;
-      *PhiMin = temp;
-    }
+  const double max_phi_window = 2.0;
+
+  if ( fabs(*max - *min) > max_phi_window) { /* Yes, is a PhiWrap case */
+
+    /* exchange maximum and minimum values */
+    double temp = *max;
+    *max = *min;
+    *min = temp;
+
     /* increase the value of PhiMax by 2*PI adjusting it */
-    *PhiMax = *PhiMax + 2 * PI;
-    return(ON);
+    *max += 2 * PI;
+
+    return(TRUE);
   }
 
-  return(OFF);
+  return(FALSE);
 }
 
 void* SmartAlloc(void* ptr, const int size)
@@ -41,14 +34,14 @@ void* SmartAlloc(void* ptr, const int size)
   if( ptr == NULL ) {/* first time */
     if ( (ptr = malloc(size)) == NULL ) 
       {
-	fprintf(stderr, "ERROR(common.c): No space for allocation\n");
+	fprintf(stderr, "(common.c): [ERROR] No space for allocation.\n");
 	return(NULL);
       }
   }
   
   else { /* can realloc */
     if( ( ptr = realloc (ptr, size) ) == NULL ) {
-      fprintf(stderr, "ERROR(common.c): No space for reallocation\n");
+      fprintf(stderr, "(common.c): [ERROR] No space for reallocation.\n");
       return(NULL);
     }
   }
