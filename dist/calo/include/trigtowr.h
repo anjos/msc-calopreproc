016a71a005b88163838f679bc71c70aa36158a40
rabello <rabello@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2000-06-28 16:00:00 +0000

    - Moved the prototype for free_layer() from the source file, it's now used by other sources (ring.c) - Moved the prototype for verify_tt() from the source file, it's now used by other sources (ring.c) - Moved the prototype for is_middle() from the source file, it's now used by other sources (ring.c)
    git-svn-id: file:///Users/andre/andre_msc/trunk/calo-preproc@95 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- dist/calo/include/trigtowr.h
+++ dist/calo/include/trigtowr.h
@@ -1,6 +1,6 @@
 /* Hello emacs, this is -*- c -*- */
 
-/* $Id: trigtowr.h,v 1.2 2000/05/31 13:57:12 rabello Exp $ */
+/* $Id: trigtowr.h,v 1.3 2000/06/28 16:00:00 rabello Exp $ */
 
 #ifndef _TRIGTOWR_H
 #define _TRIGTOWR_H
@@ -15,8 +15,30 @@ const static int MaxNumberOfLayers = 8;
 const static double EtaTTSize = 0.1;
 const static double PhiTTSize = 0.1;
 
+/* Using the ROI, define by CERN specification files, build, optinally fixing
+   the window size, a CaloTTEMRoI */
+ErrorCode build_roi(const ROI*, const bool_t, CaloTTEMRoI*);
 
-ErrorCode BuildCaloTTS(const ROI*, const bool_t, CaloTTEMRoI*);
-void FreeCaloEMRoI(CaloTTEMRoI*);
+/* Free an *used* CaloTTEMRoI */
+void free_roi(CaloTTEMRoI*);
+
+/* Free an *used* CaloLayer */
+void free_layer(CaloLayer*);
+
+/* Dumps layer(s) from a CaloTTEMRoI. For the time being, it can only dump the
+   second EM layer, from the endcap or barrel. */
+bool_t print_roi(FILE*, const CaloTTEMRoI*);
+
+/* This function just checks the second EM layer on a particular trigger tower
+   and return TRUE if it finds one and FALSE otherwise. */
+bool_t verify_tt(const CaloTriggerTower*);
+
+/* This function only verifies if this is an EM middle layer */
+bool_t is_middle(const Calorimeter, const int);
 
 #endif /* _TRIGTOWR_H */
+
+
+
+
+
