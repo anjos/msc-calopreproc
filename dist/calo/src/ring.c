a826a662e653296de21cc56f368bbb3dbe9b38ad
rabello <rabello@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2000-07-12 04:34:00 +0000

    - Added more comments to ring_sum_around() and changed its prototype - Changed ring_sum to be able to work with multiple layers at once - Added dump_ring() which simplyfies ring creation and dumping
    git-svn-id: file:///Users/andre/andre_msc/trunk/calo-preproc@125 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- dist/calo/src/ring.c
+++ dist/calo/src/ring.c
@@ -1,6 +1,9 @@
 /* Hello emacs, this is -*- c -*- */
 
-/* $Id: ring.c,v 1.2 2000/07/07 18:50:09 rabello Exp $ */
+/* $Id: ring.c,v 1.3 2000/07/12 04:34:00 rabello Exp $ */
+
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "ring.h"
 #include "ttdef.h"
@@ -8,10 +11,38 @@
 #include "trigtowr.h"
 #include "uniform.h"
 
-int get_max_idx(const CaloLayer*);
-ring_t* ring_sum_around (const CaloLayer*, const int, ring_t*);
-bool_t put_ring(ring_t*, const Energy, int);
+/* I can only accept this types of calorimeter */
+typedef enum ringcalo_t {PS=PSBARRREL, EM=EMBARREL, HAD=TILECAL} ringcalo_t;
 
+int get_max_idx(const CaloLayer*);
+ring_t* ring_sum_around (const CaloLayer*, ring_t*, const int);
+bool_t put_ring(ring_t*, const Energy, int);
+bool_t free_ring_vector (ring_t*, const int);
+void print_ring_vector (FILE*, const ring_t*, const int);
+void alloc_feature_space(ring_t*, const ringcalo_t, const LayerLevel level);
+
+void dump_ring (FILE* fp, const tt_roi_t* roi)
+{
+  ring_t* rp = (ring_t*) calloc(1,sizeof(ring_t));
+  int nring; /* the number of rings produced */
+
+  nring = ring_sum (roi, &rp); /* I have to change the value pointed by rp */
+  if (nring > 0) {
+    print_ring_vector (fp, rp, nring);
+    free_ring_vector(rp, nring);
+  }
+
+  return;
+}
+
+/* This functions prints the ring vector pointed by rp and containing nring
+   elements. The output goes to the file pointed by fp. */
+void print_ring_vector (FILE* fp, const ring_t* rp, const int nring)
+{
+  int i; /* iterator */
+  for(i=0; i<nring; ++i) print_ring(fp, &rp[i]);
+  return;
+}
 
 void print_ring (FILE* fp, const ring_t* rp)
 {
@@ -24,31 +55,40 @@ void print_ring (FILE* fp, const ring_t* rp)
 }
 
 
-ring_t* ring_sum (const tt_roi_t* r, ring_t* ring)
+int ring_sum (const tt_roi_t* r, ring_t* ring[])
 {
   int max;
   uniform_roi_t ur;
+  int i; /* iterator */
 
   ur.nlayer = 0;
   ur.layer = NULL;
-  
+
   /* Transform the tt_roi_t in succesive layers of a uniform TT*/
-  uniformize(r,&ur);
+  if (uniformize(r,&ur) != NULL) {
 
-  /* Search for the highest energy value. For now working only with one layer
-   */ 
-  max = get_max_idx(&ur.layer[0]);
+    (*ring) = (ring_t*) calloc(ur.nlayer, sizeof(ring_t));
 
-  /* Evaluate the rings */
-  ring_sum_around(&ur.layer[0],max,ring);
-  
-  free_uniform_roi(&ur);
+    for (i=0; i<ur.nlayer; ++i) {
+      /* Search for the highest energy value. */
+      max = get_max_idx(&ur.layer[i]);
 
-  return (ring);
+      alloc_feature_space(&(*ring)[i], ur.layer[i].calo, ur.layer[i].level);
+
+      /* Evaluate the rings */
+      ring_sum_around(&ur.layer[i],&(*ring)[i],max);
+    }
+
+    free_uniform_roi(&ur);
+
+    return (ur.nlayer); /* freeing will not erase nlayer! */
+  }
+
+  return 0;
 }
 
 /* This function only evaluates the maximum energy peak and return the results
-   to the caller */
+   to the caller. */
 int get_max_idx(const CaloLayer* l)
 {
   int eta, phi; /* iterators */
@@ -64,17 +104,74 @@ int get_max_idx(const CaloLayer* l)
   return linear_idx;
 }
 
-/* Evaluates the sum around the peak energy, given by the second argument. This
-   function shall allocate the space for the numbers in ring_t. The user *MUST*
-   free this afterwards, using free_ring(). */
-ring_t* ring_sum_around (const CaloLayer* l, const int max, ring_t* ring)
+/* This function will allocate space for ring features depending on the type of
+   calorimeter we are talking about. This shall limit the number of features
+   per layer. Remember that features that would exceed the normal number of
+   features are dumped to the last features and that layers that do not fulfill
+   the number of features required will have the remaining features set to
+   zero. */
+void alloc_feature_space(ring_t* rp, const ringcalo_t calo, 
+			 const LayerLevel level)
+{
+  switch(calo) {
+  case PS:
+    rp->feat = (Energy*) calloc (6,sizeof(Energy));
+    rp->nfeat = 6;
+    break;
+
+  case EM:
+    switch(level) {
+    case 1:
+      rp->feat = (Energy*) calloc (33,sizeof(Energy));
+      rp->nfeat = 33;
+      break;
+
+    case 2:
+      rp->feat = (Energy*) calloc (10,sizeof(Energy));
+      rp->nfeat = 10;
+      break;
+
+    case 3:
+      rp->feat = (Energy*) calloc (9,sizeof(Energy));
+      rp->nfeat = 9;
+      break;
+
+    default:
+      fprintf(stderr, "(ring)ERROR: EM layer level %d is wrong\n",level);
+      exit(EXIT_FAILURE);
+    }
+    break;
+
+  case HAD:  
+    rp->feat = (Energy*) calloc (3,sizeof(Energy));
+    rp->nfeat = 3;
+    break;
+
+  default:
+      fprintf(stderr, "(ring)ERROR: calo %d is wrong\n",calo);
+      exit(EXIT_FAILURE);
+  }
+
+  return;
+}
+
+
+/* Evaluates the sum around the peak energy, given by the third argument. This
+   function shall not allocate the space for the numbers in ring_t, since it is
+   the only one who knows have many features to get from each layer. The number
+   of rings is limited by the number of allocated positions within the ring. If
+   the number of rings is not filled up the remaining positions are filled up
+   with zeroes (actually unchanged) if the number of rings if filled up before
+   the end of layer processing, the remaining cell energies are added to the
+   last ring. */
+ring_t* ring_sum_around (const CaloLayer* l, ring_t* ring, const int max)
 {
   int x1,x2,y1,y2; /* helpers for placement */
   int nf = 0; 
   
   /* Place the cell with maximum energy at the front */
-  ring->nfeat = 0;
-  put_ring(ring,l->cell[max].energy, nf++);
+  put_ring(ring,l->cell[max].energy, nf); /* nf == 0 */
+  ++nf;
   
   /* set the helpers in order to start */
   x1 = l->cell[max].index.eta;
@@ -100,7 +197,8 @@ ring_t* ring_sum_around (const CaloLayer* l, const int max, ring_t* ring)
     for (p = 0; p <= nf-1; ++p)
       put_ring(ring,-ring->feat[p],nf);
     
-    ++nf;
+    /* lock the value of nf to at maximum ring->nfeat */
+    if (ring->nfeat > nf) ++nf;
 
     if (x1 == 0 && x2 == (l->EtaGran-1) && 
 	y1 == 0 && y2 == (l->PhiGran-1) ) /* I've evaluated it all! */
@@ -113,35 +211,26 @@ ring_t* ring_sum_around (const CaloLayer* l, const int max, ring_t* ring)
 
 /* This function only inserts an energy value into the ring output given by the
    first argument. The second argument is the energy to add and the third, the
-   ring to put it in. This function will allocate more and more space as it
-   needs in order to do its job. Placing a cell energy where there's already
-   something is OK, the function will sum the values, instead of allocating
-   more space (case of new rings). */
+   ring to put it in. This function will *NOT* allocate space for new
+   rings. Instead, it will gather energies into the last ring. Placing a cell
+   energy where there's already space (nr<f->nfeat), the function will sum the
+   values. */
 bool_t put_ring(ring_t* r, const Energy e, int nr)
 {
-  if (r->nfeat == 0) /* first allocation */ {
-    r->feat = (Energy*) mxalloc(NULL,1,sizeof(Energy));
-    r->feat[0] = e;
-    r->nfeat = 1;
-    return (TRUE);
-  }
-  
-  /* else we have to check the need for reallocation of memory */
-  if ( nr < r->nfeat ) /* no need for realloc, just adding */
+  if (r->nfeat > nr) /* I put the energy where it should be */
     r->feat[nr] += e;
-
-  else {/* In this case the caller asked me to allocate in a new feature */
-    if ( nr != r->nfeat ) /* Is it asking to allocate 2 or more new feats? */
-      return (FALSE);
-  
-    /* If everything goes smooth, I can realloc here */
-    r->feat = (Energy*) mxalloc(r->feat, ++r->nfeat, sizeof(Energy));
-    r->feat[nr] = e;
-    
-  }
-  
+  else /* I have to gather into the last cell */
+    r->feat[r->nfeat-1] += e;
   return (TRUE);
-  
+}
+
+/* This function frees a vector of rings pointed by r and having nring 
+   elements. */ 
+bool_t free_ring_vector (ring_t* r, const int nring)
+{
+  int i; /* iterator */
+  for (i=0; i<nring; ++i) free_ring(&r[i]);
+  return (TRUE);
 }
 
 bool_t free_ring (ring_t* r)
