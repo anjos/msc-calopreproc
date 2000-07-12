c332a509e1695fc8a46d7caffe1cd3d9335dbc9e
rabello <rabello@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2000-07-12 04:32:21 +0000

    - Included declaration of dump_ring()
    git-svn-id: file:///Users/andre/andre_msc/trunk/calo-preproc@124 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- dist/calo/include/ring.h
+++ dist/calo/include/ring.h
@@ -1,6 +1,6 @@
 /* Hello emacs, this is -*- c -*- */
 
-/* $Id: ring.h,v 1.2 2000/07/07 18:26:23 rabello Exp $ */
+/* $Id: ring.h,v 1.3 2000/07/12 04:32:21 rabello Exp $ */
 
 #ifndef _RING_H
 #define _RING_H
@@ -16,17 +16,17 @@ typedef struct ring_t
 
 /* Given an array of cells in tt_roi_t format, this function can output the
    squared rings formed by summing the rings outside the energy peak of the
-   layer being analysed. For now, this function can only work with the second
-   EM layer (endcap or barrel).
+   layer being analysed. 
 
    In order to do its job, this function will search for the highest energy
    value among all cells, and will continously sum the cells around the energy
-   peack till there are no more cells to sum.
+   peak till there are no more cells to sum or the maximum number of rings is
+   reached as described above ring.c:ring_sum_around()
 
-   The function should return a vector of energy sums based on the above
-   algorithm and the number of rings, encapsulated in a ring_t structure. The
-   space should no be preallocated, but one must free it after usage. */
-ring_t* ring_sum (const tt_roi_t*, ring_t*);
+   The function should the number of layers were the ringing algorithm were
+   applied. The space should no be preallocated, but one must free it after
+   usage. */ 
+int ring_sum (const tt_roi_t*, ring_t**);
 
 /* This function just frees a ring_t */
 bool_t free_ring (ring_t*);
@@ -34,5 +34,7 @@ bool_t free_ring (ring_t*);
 /* Prints the features pointed by rp in fp in line format */
 void print_ring (FILE*, const ring_t*);
 
+/* Dumps to the given file, the roi in form of rings */
+void dump_ring (FILE*, const tt_roi_t*);
 
 #endif /*_RING_H*/
