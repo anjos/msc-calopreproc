9277001d794168e27bfa885906fcf07180382c4c
rabello <rabello@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2000-08-11 20:30:32 +0000

    - Added a compilation macro to the command lines: _GNU_SOURCE
    git-svn-id: file:///Users/andre/andre_msc/trunk/calo-preproc@157 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- dist/calo/mk/config.mk
+++ dist/calo/mk/config.mk
@@ -2,13 +2,13 @@
 # include this file in your makefiles. Doing this you'll guarantee
 # the use of the same compilation flags for each object created.
 #
-# $Id: config.mk,v 1.3 2000/06/28 15:57:56 rabello Exp $
+# $Id: config.mk,v 1.4 2000/08/11 20:30:32 rabello Exp $
 
 AR = ar
 CC = gcc
 RANLIB = ranlib
 ARFLAGS = -rvu
-CDEFS = -D_BSD_SOURCE
+CDEFS = -D_BSD_SOURCE -D_GNU_SOURCE
 CFLAGS = -g -ansi -pedantic -Wall $(CDEFS)
 MAKE = gmake
 RM = rm -f
