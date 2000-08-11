38b0f766999c48e4462c47c6d3315dc90218bab3
rabello <rabello@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2000-08-11 16:12:36 +0000

    - Altered ring_sum() to be able to adjust it to the fact that one should not   process things that are not going to be printed. This is true in this case   since ring processing is independent from cut parameters as in uniformize(). - Altered ring_sum() replacing the double pointer parameter to ring_t into a   simplified structure now declared into the module header. This was done to   simplify pointer updating outside the module. Using double pointers is   *REALLY* confusing sometimes. - Replaced alloc_feature_space() by number_of_features() which only returns the   number of features to allocate for a given layer info. This makes it   necessary to allocate space on the caller function.
    git-svn-id: file:///Users/andre/andre_msc/trunk/calo-preproc@148 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- dist/calo/src/ring.c
+++ dist/calo/src/ring.c
@@ -1,6 +1,6 @@
 /* Hello emacs, this is -*- c -*- */
 
-/* $Id: ring.c,v 1.4 2000/07/20 00:45:37 rabello Exp $ */
+/* $Id: ring.c,v 1.5 2000/08/11 16:12:36 rabello Exp $ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,7 +20,7 @@ typedef enum ringcalo_t {PS=PSBARRREL, EM=EMBARREL, HAD=TILECAL} ringcalo_t;
 int get_max_idx(const CaloLayer*);
 ring_t* ring_sum_around (const CaloLayer*, ring_t*, const int);
 bool_t put_ring(ring_t*, const Energy, int);
-void alloc_feature_space(ring_t*, const ringcalo_t, const LayerLevel level);
+int number_of_features(const ringcalo_t, const LayerLevel level);
 
 int fprintf_ring_vector (FILE* fp, const ring_t* rp, const int nring)
 {
@@ -88,35 +88,59 @@ int asprintf_ring (char** sp, const ring_t* rp)
   return (rp->nfeat);
 }
 
-int ring_sum (const tt_roi_t* r, ring_t* ring[], 
-	      const unsigned short uflags, 
-	      const unsigned short nflags)
+int ring_sum (const tt_roi_t* r, ringroi_t* ringroi,
+	      const unsigned short layer_flags, 
+	      const unsigned short print_flags, 
+	      const unsigned short normalization_flags)
 {
   int max;
   uniform_roi_t ur;
   int i; /* iterator */
+  int rc = 0; /* ring counter - iterator */
 
   ur.nlayer = 0;
   ur.layer = NULL;
 
   /* Transform the tt_roi_t in succesive layers of a uniform TT*/
-  if (uniformize(r,&ur,uflags,nflags) != NULL) {
+  if (uniformize(r,&ur,layer_flags,normalization_flags) != NULL) {
 
-    (*ring) = (ring_t*) calloc(ur.nlayer, sizeof(ring_t));
+    /* Pre-allocate the number of rings that should be enough for all the
+       output */
+    ringroi->nring = flag_contains_nlayers(print_flags);
+    ringroi->ring = (ring_t*) calloc(ringroi->nring, sizeof(ring_t));
 
+    /* loop over all layers. If layer is not to be printed, it should be
+       ignored. Attention: do *NOT* use i for ring indexing since it contains
+       the layer indexing which is not the same for rings. Layers are selected
+       according to the layer_flag, while rings are created depending on the
+       print_flag! */
     for (i=0; i<ur.nlayer; ++i) {
-      /* Search for the highest energy value. */
-      max = get_max_idx(&ur.layer[i]);
 
-      alloc_feature_space(&(*ring)[i], ur.layer[i].calo, ur.layer[i].level);
+      /* Only process the layers that are going to be printed */
+      if ( flag_contains_layer(print_flags, &ur.layer[i]) ) {
+	
 
-      /* Evaluate the rings */
-      ring_sum_around(&ur.layer[i],&(*ring)[i],max);
+	/* Search for the highest energy value. */
+	max = get_max_idx(&ur.layer[i]);
+
+	/* Allocate space for features and adjust ring parameters */
+	ringroi->ring[rc].nfeat = number_of_features( ur.layer[i].calo, 
+						      ur.layer[i].level );
+	ringroi->ring[rc].feat = (Energy*) calloc ( ringroi->ring[rc].nfeat,
+						    sizeof(Energy) );
+
+	/* Evaluate the rings */
+	ring_sum_around(&ur.layer[i],&(ringroi->ring[rc]),max);
+
+	/* Let's process the next ring on the next pass */
+	++rc; 
+      }
+
     }
 
     free_uniform_roi(&ur);
 
-    return (ur.nlayer); /* freeing will not erase nlayer! */
+    return (flag_contains_nlayers(print_flags));
   }
 
   return 0;
@@ -139,37 +163,28 @@ int get_max_idx(const CaloLayer* l)
   return linear_idx;
 }
 
-/* This function will allocate space for ring features depending on the type of
-   calorimeter we are talking about. This shall limit the number of features
-   per layer. Remember that features that would exceed the normal number of
-   features are dumped to the last features and that layers that do not fulfill
-   the number of features required will have the remaining features set to
-   zero. */
-void alloc_feature_space(ring_t* rp, const ringcalo_t calo, 
-			 const LayerLevel level)
+/* This function will return the number of features one should allocate for
+   ring features depending on the type of calorimeter we are talking
+   about. This shall limit the number of features per layer. Remember that
+   features that would exceed the normal number of features are dumped to the
+   last features and that layers that do not fulfill the number of features
+   required will have the remaining features set to zero. */
+int number_of_features(const ringcalo_t calo, const LayerLevel level)
 {
   switch(calo) {
   case PS:
-    rp->feat = (Energy*) calloc (6,sizeof(Energy));
-    rp->nfeat = 6;
-    break;
+    return 6;
 
   case EM:
     switch(level) {
     case 1:
-      rp->feat = (Energy*) calloc (33,sizeof(Energy));
-      rp->nfeat = 33;
-      break;
+      return 33;
 
     case 2:
-      rp->feat = (Energy*) calloc (10,sizeof(Energy));
-      rp->nfeat = 10;
-      break;
+      return 10;
 
     case 3:
-      rp->feat = (Energy*) calloc (9,sizeof(Energy));
-      rp->nfeat = 9;
-      break;
+      return 9;
 
     default:
       fprintf(stderr, "(ring)ERROR: EM layer level %d is wrong\n",level);
@@ -178,16 +193,15 @@ void alloc_feature_space(ring_t* rp, const ringcalo_t calo,
     break;
 
   case HAD:
-    rp->feat = (Energy*) calloc (3,sizeof(Energy));
-    rp->nfeat = 3;
-    break;
+    return 3;
 
   default:
       fprintf(stderr, "(ring)ERROR: calo %d is wrong\n",calo);
       exit(EXIT_FAILURE);
   }
 
-  return;
+  /* if you got here, you have trouble! */
+  return 0; 
 }
 
 
