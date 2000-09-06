0480fda4b244411916afad0acdb6be77ecb89fdc
rabello <rabello@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2000-09-06 21:13:36 +0000

    - Changed a bit the call to uniform_roi_normalize(). The second argument got an   'unsigned' parameter forcing it to an unsigned short.
    - Added the call to ring_normalize() that will act just like
      uniform_roi_normalize().
    
    
    git-svn-id: file:///Users/andre/andre_msc/trunk/calo-preproc@210 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- dist/calo/include/normal.h
+++ dist/calo/include/normal.h
@@ -1,18 +1,25 @@
 /* Hello emacs, this is -*- c -*- */
 /* André Rabello dos Anjos <Andre.Rabello@ufrj.br> */
 
-/* $Id: normal.h,v 1.1 2000/09/06 14:58:45 andre Exp $ */
+/* $Id: normal.h,v 1.2 2000/09/06 21:13:36 rabello Exp $ */
 
 #ifndef __NORMAL_H
 #define __NORMAL_H
 
 #include "uniform.h"
 #include "common.h"
+#include "ring.h"
 
-/* Just divides all energy values on the pointer to the uniform RoI by the
-   energy value contained in nfactor. OBS: the uniform RoI is changed during
-   this call. */
-void uniform_roi_normalize (uniform_roi_t*, const short);
+/* This function is a front end to the uniform_roi_t type. It will apply the
+   correct type of normalization to the variable or dunno, if no normalization
+   is demanded. */
+void uniform_roi_normalize (uniform_roi_t*, const unsigned short*);
+
+/* This function is the frontend to ringroi_t normalization. It will select the
+   correct function call to apply in each occasion or dunno if asked so. The
+   first argument is the ring RoI you have created. The second, a set of
+   normalization flags, produced by string2normalization() bellow. */
+void ring_normalize(ringroi_t*, const unsigned short*);
 
 /* Converts the string token to one of the possible normalization parameters,
    defined on the body of uniform.c:normal_t. This function checks
