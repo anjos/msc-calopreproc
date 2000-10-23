8c429f0fe32e4426438aeb85690eae47389d08f4
andre <andre@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2000-10-23 02:24:30 +0000

    - Added 2 helper functions that can evaluated the energy (inside a ringroi) of   layers or whole RoI. - Changed ring_sum() to reflect the adds of 2 types of normalization on   normal.h - Created type for configuring the new ring normalization types. It had to be   put here, since normal.c _and_ ring.c had to look at it simultaneously.
    git-svn-id: file:///Users/andre/andre_msc/trunk/calo-preproc@234 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- dist/calo/include/ring.h
+++ dist/calo/include/ring.h
@@ -1,6 +1,6 @@
 /* Hello emacs, this is -*- c -*- */
 
-/* $Id: ring.h,v 1.8 2000/09/19 00:33:44 andre Exp $ */
+/* $Id: ring.h,v 1.9 2000/10/23 02:24:30 andre Exp $ */
 
 #ifndef _RING_H
 #define _RING_H
@@ -23,6 +23,17 @@ typedef struct ringroi_t
   int nring;
 }ringroi_t;
 
+/* The next type configures the limit for each segment in terms of weighted
+   normalization. After the configured limit, the remaining features are
+   normalized using a constant factor. As stated by Igor's work. This has to be
+   public, so the user can use it for configuration purposes. I had to put this
+   thing over here, so both ring.c and normal.h can use it... No other way */
+typedef struct config_weighted_t
+{
+  unsigned short nlayers;
+  unsigned short* last2norm;
+}config_weighted_t;
+
 /* Given an array of cells in uniform_roi_t format, this function can output
    the squared rings formed by summing the rings outside the energy peak of the
    layer being analysed. The RoI on it's uniformized state (by uniform.c)
@@ -43,11 +54,14 @@ typedef struct ringroi_t
    preallocated. I suggest using static local allocation.
 
    This function will also apply the normalization (if needed) to the newly
-   formed ring structure of values. For that, the last argument should be
+   formed ring structure of values. For that, the fifth argument should be
    passed with the value of the maximum radius value for 'unity+'
-   normalization, as describe by the 'normal' module. */
+   normalization, as describe by the 'normal' module. The last argument will be
+   used in the case of weighted normalization, as exposed in Igor's work. It's
+   a structured described in normal.h that contains the last rings that will
+   have varying normalization factors. */
 int ring_sum (const uniform_roi_t*, ringroi_t*, const unsigned short*,
-	      const unsigned short*, const Energy*);
+	      const unsigned short*, const Energy*, const config_weighted_t*);
 
 /* This function just frees a ring_t */
 bool_t free_ring (ring_t*);
@@ -75,6 +89,12 @@ int asprintf_ring (char**, const ring_t*);
 /* This function frees a vector of rings pointed by r and having nring 
    elements. */ 
 bool_t free_ring_vector (ring_t*, const int);
+
+/* Calculates the energy in a ring layer, returning the result on *e */
+void ringlayer_energy(const ring_t*, Energy*);
+
+/* Calculates the total RoI energy, based on rings only */
+void ringroi_energy(const ringroi_t*, Energy*);
 
 #endif /*_RING_H*/  
 
