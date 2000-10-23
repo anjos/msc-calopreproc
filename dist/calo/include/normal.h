a3ad72fd3c4bfc37df2fee2b7ab2b66dc6348345
andre <andre@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2000-10-23 02:24:09 +0000

    - Added support for the 2 new normalization types in normal.c - Added more comments to ring_normalize()
    git-svn-id: file:///Users/andre/andre_msc/trunk/calo-preproc@233 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- dist/calo/include/normal.h
+++ dist/calo/include/normal.h
@@ -1,7 +1,7 @@
 /* Hello emacs, this is -*- c -*- */
 /* André Rabello dos Anjos <Andre.Rabello@ufrj.br> */
 
-/* $Id: normal.h,v 1.4 2000/09/19 00:33:22 andre Exp $ */
+/* $Id: normal.h,v 1.5 2000/10/23 02:24:09 andre Exp $ */
 
 #ifndef __NORMAL_H
 #define __NORMAL_H
@@ -16,12 +16,16 @@
 void uniform_roi_normalize (uniform_roi_t*, const unsigned short*);
 
 /* This function is the frontend to ringroi_t normalization. It will select the
-   correct function call to apply in each occasion or dunno if asked so. The
-   first argument is the ring RoI you have created. The second, a set of
-   normalization flags, produced by string2normalization() bellow. The third
-   argument is option and may be applied only if "unity+" normalization is
-   required. It represents the sphere radius of normalization. */
-void ring_normalize(ringroi_t*, const unsigned short*, const Energy*);
+   correct function call to apply in each occasion or dunno if asked so. Note
+   that the first argument is mandatory in all situations since the process
+   requires the rings to be there, also the normalization type. The third and
+   fourth parameters are there for some specific methods: 
+   3rd parameter -> UNITY+ normalization, indicating the normalization radius
+   4th parameter -> WEIGTHED normalization stop, indicating the maximum ring to
+   normalize with changing parameters, meaning the next rings will be
+   normalized according a constant principle. */
+void ring_normalize(ringroi_t*, const unsigned short*, const Energy*,
+		    const config_weighted_t*);
 
 /* Converts the string token to one of the possible normalization parameters,
    defined on the body of uniform.c:normal_t. This function checks
@@ -42,5 +46,10 @@ bool_t normal_is_unity(const unsigned short*);
 /* This function returns true if the unity+ normalization flag is active,
    meaning that ring-unity-normalization will be applied. */
 bool_t normal_is_unityx(const unsigned short*);
+
+/* These next 2 functions test whether we have one of the normalizations
+   described. */
+bool_t normal_is_weighted_seg(const unsigned short*);
+bool_t normal_is_weighted_all(const unsigned short*);
 
 #endif /* __NORMAL_H */
