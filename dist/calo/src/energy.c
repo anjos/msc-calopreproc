4552fb0400bd29fb1cceddba0fa769649686e1e5
rabello <rabello@b2e9302d-b4d2-0310-80c4-8586bce995ec>
2000-11-28 21:03:11 +0000

    - Changed uniform_roi_classics() to extract the L2 quantities directly from uniform_roi_t rather than taking them from the RoI header. No testing has been done, this is a preview release. - Included functions for the calculation of L2 quantities. Such functions calculate Et37, Rshape and Rstrip, as defined by the Trigger TPR.
    git-svn-id: file:///Users/andre/andre_msc/trunk/calo-preproc@268 b2e9302d-b4d2-0310-80c4-8586bce995ec

--- dist/calo/src/energy.c
+++ dist/calo/src/energy.c
@@ -3,18 +3,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include "energy.h"
 #include "data.h"
 #include "uniform.h"
 #include "common.h"
 
-/* $Id: energy.c,v 1.6 2000/11/25 19:38:18 andre Exp $ */
+/* $Id: energy.c,v 1.7 2000/11/28 21:03:11 rabello Exp $ */
 
 /* Some prototypes used here on */
 Energy* energy_from_all_digis(const ROI*, Energy*);
 Energy* uniform_layer_energy (const CaloLayer*, Energy*);
 void uniform_roi_classics(const ROI*, const uniform_roi_t*, char**);
+
+/* Functions for classical feature extraction */
+void build_etem (const uniform_roi_t*, const int*, double*);
+void build_rshape (const CaloLayer*, const int*, double*);
+void build_rstrip (const CaloLayer*, const int*, double*);
 
 /* These are the shorts that define what we'll dump. Each of those is explained
    above. */
@@ -58,8 +64,7 @@ unsigned short* string2edump(unsigned short* to, const char* from)
     else if ( strcasecmp(token,"roi_etem") == 0 ) (*to) |= EDUMP_ROI_ETEM;
     else if ( strcasecmp(token,"roi_ethad") == 0 ) (*to) |= EDUMP_ROI_ETHAD;
     else if ( strcasecmp(token,"roi_digis") == 0 ) (*to) |= EDUMP_ROI_DIGIS;
-    else if ( strcasecmp(token,"classics") == 0 ) 
-      (*to) |= (EDUMP_CLASSICS | EDUMP_DB_ETHAD);
+    else if ( strcasecmp(token,"classics") == 0 ) (*to) |= (EDUMP_CLASSICS);
     else if ( strcasecmp(token,"all") == 0 ) {
       (*to) = EDUMP_ALL;
       break;
@@ -91,8 +96,7 @@ unsigned short* string2edump(unsigned short* to, const char* from)
    8) CLASSICS - Will dump the classical features for e/jet discrimination, as
                  defined by the ATLAS Trigger TPR. Those are basically cell
 		 sums in different configurations. There 4 features on the
-		 grand total. The first 2 are 1) and 2) from this listing the
-		 others are described on the Trigger Performance TPR.
+		 grand total.
 */ 
 char* get_energy(const ROI* r, const uniform_roi_t* ur,
 		 const unsigned short* flags, const char* initstring)
@@ -179,7 +183,7 @@ char* edump2string (const unsigned short* from, char* to)
   if (*from & EDUMP_ROI_ETEM) ascat(&retval,"ROI_ETEM");
   if (*from & EDUMP_ROI_ETHAD) ascat(&retval,"ROI_ETHAD");
   if (*from & EDUMP_ROI_DIGIS) ascat(&retval,"ROI_DIGIS");
-  if (*from & EDUMP_CLASSICS) ascat(&retval,"CLAS_ET37 CLAS_RCORE CLAS_RSTRIP");
+  if (*from & EDUMP_CLASSICS) ascat(&retval,"CLAS_ET37 CLAS_ETHAD CLAS_RCORE CLAS_RSTRIP");
 
   /* final delimiter */
   ascat(&retval,")");
@@ -226,6 +230,15 @@ bool_t validate_energy_selection(const unsigned short* layer_flags,
       fprintf(stderr, " selected anything\n");
       *energy_flags = EDUMP_NONE;
       fprintf(stderr, "(energy) I won't print URoI energies\n");
+    }
+
+  if (*energy_flags & EDUMP_CLASSICS)
+    if ( (*layer_flags) & FLAG_ALL) {
+      fprintf(stderr, "(energy) You can't demand classical features ");
+      fprintf(stderr, "extraction if you did not selected PS, EM1-3, ");
+      fprintf(stderr, "HAD1-3\n");
+      *energy_flags &= ~EDUMP_CLASSICS;
+      fprintf(stderr, "(energy) I won't print CLASSICAL features\n");
     }
 
   /* always return TRUE here, no harm in doing that */
@@ -338,49 +351,192 @@ Energy* uniform_layer_energy (const CaloLayer* lp, Energy* counter)
 
 /* This function is actually, just a front end to the functions that will
    perform classical feature extraction. The first parameter is the ROI, as
-   seen by level 2, depending on implementationg, this function may take the
-   values from the RoI header directly, process the 'raw' RoI to get them or
-   process the uniformized RoI. Anyway, results are print on (*retval), the
-   last argument. The user should not note differences between direct RoI
-   header access or calculations, with matter to the interface usage.
-*/
+   seen by level 2. This routine will use only the first argument to calculate
+   the quantities. Following works may compare this results with the ones
+   extracted from the 'uniform_roi_t'. Anyway, results are print on (*retval),
+   the last argument. .  */
 void uniform_roi_classics(const ROI* r, const uniform_roi_t* ur, char** retval)
 {
   Energy temp;
+  int max;
+  double eta;
+  double phi;
+  int i; /* iterator */
 
   /* I */
-  /* This energy is assumed to be PS+EM cells into a 3x7 (etaxphi) region
-     around the peak of energy. The values are corrected according to a formula
-     shown on TPR. Now, I'll take for granted that this is the same as the
-     quantity Et_EM, since I can't see why to have other value for the same
-     stuff. */
-  temp = r->l2CalEm.Et37;
-  ascat_double(retval, &temp);
+  /* Find the peak of energy on EM2 and put it into retval */
+  for (i=0; i<ur->nlayer; ++i) {
+    if (ur->layer[i].calo == EM && ur->layer[i].level == 2) 
+      peak_find(ur->layer, &max, &eta, &phi);
+  }
+/*    ascat_double(retval, &eta); */
+/*    ascat_phi(retval, &phi); */
 
   /* II */
-  /* ------ ETHAD IS DUMPED BY THE DEFAULT PROCEDURE UPSTAIRS ------ */
+  /* This energy is assumed to be PS+EM cells into a 3x7 (etaxphi) region
+     around the peak of energy. The values should be corrected according to a
+     formula shown on TPR, but since for discriminating purposes this is *not*
+     needed (correction is linear) we won't apply it.
+  */
+  build_etem(ur, &max, &temp);
+  ascat_double(retval, &temp);
+
+  /* III */
   /* This is actually the same as above, but taken for the Hadronic
      Calorimeter. The quantity is used with predefined cuts, that are used
      according to the object Et (calculated on the first step). For Et till
      25.5GeV, the cut is 2.2, till 60GeV, 4.0 and abover 120Gev the cut used is
      1000 (no cut). */
-
-  /* III */
-  /* This measures the lateral shape of the current object. This quantity is
-     not influenced directly by the objects' Et, but there should be caution
-     using it with different objects. */
-  temp = r->l2CalEm.Etlay[2] / r->l2CalEm.Et77lay[2];
+  uniform_roi_HAD_energy(ur, &temp);
   ascat_double(retval, &temp);
 
   /* IV */
+  /* This measures the lateral shape of the current object. This quantity is
+     not influenced directly by the objects' Et, but there should be caution
+     using it with different objects. */
+  for (i=0; i<ur->nlayer; ++i) {
+    if (ur->layer[i].calo == EM && ur->layer[i].level == 2) 
+      build_rshape(&ur->layer[i], &max, &temp);
+  }
+  ascat_double(retval, &temp);
+
+  /* V */
   /* The last quantity measures the lateral shape in the first sampling. This
      cut is applied by last and therefore is more "fine" than the others. You
      have to tune it correctly in order to get the most out of it. */
-  temp = (r->l2CalEm.E1st - r->l2CalEm.E2nd) /
-    (r->l2CalEm.E1st + r->l2CalEm.E2nd);
+  for (i=0; i<ur->nlayer; ++i) {
+    if (ur->layer[i].calo == EM && ur->layer[i].level == 1 ) 
+      build_rstrip(&ur->layer[i], &max, &temp);
+  }
   ascat_double(retval, &temp);
 
   return;
 }
 
+/* This function will build the EtEM quantity on a 3x7 region around the peak
+   of energy (found on EM layer 2). The 3x7 regions correspond to the cell
+   size of EM layer 2. For the other EM layers and the PreSampler, a comparison
+   for cell position inside the 3x7 (0.075x0.175, eta x phi) is done. If the
+   cell center falls inside this region, its added, otherwise not.
+*/
+void build_etem (const uniform_roi_t* ur, const int* max, double* val)
+{
+  int i; /* iterator */
+  int j; /* iterator */
+  double eta_max;
+  double phi_max;
+  double eta;
+  double phi;
+
+  const int stdgran_eta = 16;
+  const int stdgran_phi = 16;
+
+  const double cluster_eta_range = 3 * (0.4/stdgran_eta);
+  const double cluster_phi_range = 7 * (0.4/stdgran_phi);
+
+  /* Initializes (*val) */
+  (*val) = 0;
+
+  /* Get coordinates of the peak */
+  vector2point(&stdgran_eta, &stdgran_phi, max, &eta_max, &phi_max);
+
+  /* Sums all cells from within the prescribed range */
+  for (i=0; i<ur->nlayer; ++i)
+    if (ur->layer[i].calo == PS || ur->layer[i].calo == EM) {
+      for (j=0; j<ur->layer[i].NoOfCells; ++j) {
+	vector2point(&ur->layer[i].EtaGran, &ur->layer[i].PhiGran,
+		     &j, &eta, &phi);
+	if (fabs(eta-eta_max) <= cluster_eta_range &&
+	    fabs(eta-phi_max) <= cluster_phi_range) {
+	  (*val) += ur->layer[i].cell[j].energy;
+	}
+      }
+    }
+
+  return;
+}
+
+/* This function will build the quantity known as Rshape. It is the division of
+   the 3x7 energy found on EM layer 2 by the 7x7 energy on the same layer. Both
+   ranges are considered for the peak. Here I won't have to convert the cell
+   indexes into real numbers since will operate only on layer 2. The input
+   argument #1 will be expected to be _that_ layer. Output, as always will be
+   on the second argument. */  
+void build_rshape (const CaloLayer* layer, const int* peak, double* val)
+{
+  int j; /* iterator */
+  int eta_max;
+  int phi_max;
+  int eta;
+  int phi;
+  div_t R; /* just to get eta's and phi's */
+
+  double e37 = 0;
+  double e77 = 0;
+
+  const int cluster_eta_range_numerator = 3;
+  const int cluster_eta_range_denominator = 7;
+  const int cluster_phi_range = 7;
+
+  /* Get coordinates of the peak */
+  R = div((*peak),layer->EtaGran);
+  eta_max = R.rem;
+  phi_max = R.quot;
+
+  /* Sums all cells from within the prescribed range */
+  for (j=0; j < layer->NoOfCells; ++j) {
+    R = div(j,layer->EtaGran);
+    eta = R.rem;
+    phi = R.quot;
+    /* First constraint is that it has to be inside the phi range */
+    if (abs(phi-phi_max) <= cluster_phi_range) {
+      /* Then, if it is inside the numerator range I add to both 
+	 subquantities*/
+      if (abs(eta-eta_max) <= cluster_eta_range_numerator) {
+	e37 += layer->cell[j].energy;
+	e77 += layer->cell[j].energy;
+      }
+      /* Else, if it is still under the denominator range, I add only to the
+	 e77 quantity */
+      else if (abs(eta-eta_max) <= cluster_eta_range_denominator) {
+	e77 += layer->cell[j].energy;
+      }
+    }
+  }
+
+  if (e77 > 0) (*val) = e37/e77;
+  else (*val) = 99; /* Don't know why this, I'm following RefSoft
+		       implementation */ 
+
+  return;
+}
+
+/* This function will build the quantity known as Rstrip. This quantity is used
+   for fine jet separation (as I understood from Saul...). In order to find
+   this quantity, one searchs, in a window 0.2x0.2 around the peak of energy on
+   EM layer 2, on EM layer 1 for two strips with the maximum energy. That's an
+   easy job! */
+void build_rstrip (const CaloLayer* layer, const int* max, double* val)
+{
+  double e1 = 0;
+  double e2 = 0;
+
+  /* This is a dummy algorithm */
+  int i; /* iterator */
+
+  /* Search in a reduced region is not implemented here. Search will happen on
+     a 0.4x0.4 region. This algorithm is exaustive!!!! */
+  for (i=0; i<layer->NoOfCells; ++i) {
+    if (layer->cell[i].energy > e1) {
+      e2 = e1;
+      e1 = layer->cell[i].energy;
+    }
+  }
+
+  /* Finally, we get the quantity */
+  if (e1+e2 > 0) (*val) = (e1-e2)/(e1+e2);
+  else (*val) = 99; /* ?? */
+
+  return;
+}
 
